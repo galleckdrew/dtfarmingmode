f41651ab5826b4c0ee7047913ca5f463327192ae
Drew Galleck <galleckdrew@gmail.com>
2025-03-28 22:58:06 -0400

    Switched to bcryptjs for deployment compatibility

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const bcrypt = require("bcryptjs");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.get("/driver-form", (req, res) => {
