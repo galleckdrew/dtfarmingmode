f702f956066b75dd1c942e9535433b2693bb206f
Drew Galleck <galleckdrew@gmail.com>
2025-04-09 08:04:19 -0400

    Fix: Added missing mongoose import to Tractor model

--- models/Tractor.js
+++ models/Tractor.js
@@ -1,4 +1,8 @@
+const mongoose = require("mongoose");
+
 const tractorSchema = new mongoose.Schema({
   name: String,
-  gallons: Number // ✅ default gallons already supported
+  gallons: Number, // ✅ default gallons set by admin
 });
+
+module.exports = mongoose.model("Tractor", tractorSchema);
