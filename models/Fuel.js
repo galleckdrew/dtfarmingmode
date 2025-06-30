9dc79b8aca48ae1dfc0da6566ad3931e7542979a
Drew <galleckdrew@gmail.com>
2025-06-30 18:23:19 -0400

    Fix: add farm ref to Fuel and update driver history logic

--- models/Fuel.js
+++ models/Fuel.js
@@ -1,10 +1,30 @@
-const mongoose = require('mongoose');
+// models/Fuel.js
+const mongoose = require("mongoose");
 
 const fuelSchema = new mongoose.Schema({
-  tractor: { type: mongoose.Schema.Types.ObjectId, ref: 'Tractor', required: true },
-  field: { type: mongoose.Schema.Types.ObjectId, ref: 'Field', required: true },
-  gallons: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now }
+  tractor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Tractor",
+    required: true,
+  },
+  field: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Field",
+    required: false,
+  },
+  farm: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Farm",
+    required: false,
+  },
+  gallons: {
+    type: Number,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Fuel', fuelSchema);
+module.exports = mongoose.model("Fuel", fuelSchema);
